ffa3f941502f5ea8d922e0eb1285aaea12612dc6
pyZ6 <mansoori.shahriarr@gmail.com>
2023-07-30 18:44:37 +0330

    develop addAdvertising page

--- src/pages/add advertising/AddAdvertising.js
+++ src/pages/add advertising/AddAdvertising.js
@@ -1,26 +1,24 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useCookies } from "react-cookie";
 import { useDispatch } from "react-redux";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { getOrders } from "../../redux/orders/ordersActions";
 
 export default function AddAdvertising() {
 
-  const [cookies, setCookie, removeCookie] = useCookies(["userID"]);
   const navigate = useNavigate();
   const { state } = useLocation();
-  console.log(state)
 
+  const [totalPrice, setTotalPrice] = useState(0);
   const [options, setOptions] = useState({
-    nardeban: false,
-    urgent: false
+    nardeban: { active: false, price: 20000 },
+    urgent: { active: false, price: 40000 }
   })
 
   const dispatch = useDispatch();
 
   const optionChangeHandler = e => {
-    setOptions({ ...options, [e.target.name]: !options[e.target.name] })
+    setOptions({ ...options, [e.target.name]: { ...options[e.target.name], active: !options[e.target.name].active } })
   }
 
   const onSubmitChange = (e) => {
@@ -28,18 +26,12 @@ export default function AddAdvertising() {
 
     const obj = {
       ...state,
-      ...options
+      nardeban: options.nardeban.active,
+      urgent: options.urgent.active
     }
 
-    console.log(obj)
-    if (options.nardeban && options.urgent) {
-      console.log("USERID => ", cookies.userID, "PRICE => ", 60000)
-    } else if (options.nardeban) {
-      console.log("USERID => ", cookies.userID, "PRICE => ", 20000)
-    } else if (options.urgent) {
-      console.log("USERID => ", cookies.userID, "PRICE => ", 40000)
-    } else {
-      console.log("USERID => ", cookies.userID, "PRICE => ", 0)
+    if (totalPrice) {
+      console.log("userID => ", obj.user_id, "Total Price => ", totalPrice)
     }
 
     axios.post("http://127.0.0.1:8000/api/addorder", obj)
@@ -51,8 +43,11 @@ export default function AddAdvertising() {
     if (!state) {
       navigate("/add-advertising-details")
     }
-
   }, []);
+
+  useEffect(() => {
+    setTotalPrice(Object.keys(options).filter(key => options[key].active)?.reduce((acc, cur) => acc + options[cur].price, 0))
+  }, [options])
 
   return (
     <section id="main" className="clearfix ad-details-page">
@@ -66,7 +61,6 @@ export default function AddAdvertising() {
           </ol>
           <h2 className="title">افزودن سفارش </h2>
         </div>
-
         <div className="adpost-details">
           <div className="row">
             <div className="col-lg-8">
@@ -81,32 +75,38 @@ export default function AddAdvertising() {
                     <div className="row form-group select-option">
                       <div className="col-12">
                         <div className="select-nardeban">
-                          <label htmlFor="select-option-1" className="label-option">
-                            نردبان
-                          </label>
-                          <input
-                            type="checkbox"
-                            className="form-control"
-                            id="select-option-1"
-                            name="nardeban"
-                            onChange={(e) => optionChangeHandler(e)}
-                          />
+                          <div>
+                            <label htmlFor="select-option-1" className="label-option">
+                              نردبان
+                            </label>
+                            <input
+                              type="checkbox"
+                              className="form-control"
+                              id="select-option-1"
+                              name="nardeban"
+                              onChange={(e) => optionChangeHandler(e)}
+                            />
+                          </div>
+                          <span>{options.nardeban.price}</span>
                         </div>
                         <h5>توضیحات :</h5>
                         <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز</p>
                       </div>
                       <div className="col-12">
                         <div className="select-nardeban">
-                          <label htmlFor="select-option-2" className="label-option">
-                            فوری
-                          </label>
-                          <input
-                            type="checkbox"
-                            className="form-control"
-                            id="select-option-2"
-                            name="urgent"
-                            onChange={(e) => optionChangeHandler(e)}
-                          />
+                          <div>
+                            <label htmlFor="select-option-2" className="label-option">
+                              فوری
+                            </label>
+                            <input
+                              type="checkbox"
+                              className="form-control"
+                              id="select-option-2"
+                              name="urgent"
+                              onChange={(e) => optionChangeHandler(e)}
+                            />
+                          </div>
+                          <span>{options.urgent.price}</span>
                         </div>
                         <h5>توضیحات :</h5>
                         <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز</p>
@@ -127,37 +127,12 @@ export default function AddAdvertising() {
             </div>
             <div className="col-lg-4">
               <div className="section quick-rules">
-                <h4>قوانین</h4>
-                <p className="lead">
-                  ارسال آگهی همیشه در <a href="#">کالا</a> رایگان است هرچند
-                  تمامی آگهی‌ها باید از قوانین سایت پیروی کنند!
-                </p>
-
-                <ul>
-                  <li>حتماً در دسته صحیح پست کنید.</li>
-                  <li>
-                    بیش از یک بار آگهی مشابه را ارسال نکنید و یا ظرف 48 ساعت یک
-                    آگهی را دوباره ارسال کنید.
-                  </li>
-                  <li>تصاویر را با علامت بارگذاری نکنید.</li>
-                  <li>
-                    تبلیغات حاوی چندین مورد را ارسال نکنید ، مگر اینکه این یک
-                    بسته بندی باشد.
-                  </li>
-                  <li>
-                    ایمیل یا شماره تلفن خود را در عنوان یا توضیحات قرار ندهید.
-                  </li>
-                  <li>حتماً در دسته صحیح پست کنید.</li>
-                  <li>
-                    بیش از یک بار آگهی مشابه را ارسال نکنید و یا ظرف 48 ساعت یک
-                    آگهی را دوباره ارسال کنید.
-                  </li>
-                  <li>تصاویر را با علامت بارگذاری نکنید.</li>
-                  <li>
-                    تبلیغات حاوی چندین مورد را ارسال نکنید ، مگر اینکه این یک
-                    بسته بندی باشد.
-                  </li>
-                </ul>
+                <p>مبلغ قابل پرداخت</p>
+                <span>
+                  {
+                    totalPrice
+                  }
+                </span>
               </div>
             </div>
           </div>
